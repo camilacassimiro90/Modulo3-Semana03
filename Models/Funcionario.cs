0ab81853c3ef25dbcf808f9838d60234179ad855
Cami <camilacassimiro@gmail.com>
2022-09-16 10:57:39 -0300

    feature: [M3S03] Ex 3 - Criar classe Funcionario

---
+++ Models/Funcionario.cs
@@ -0,0 +1,13 @@
+using RH.Enums;
+
+namespace RH.Models;
+
+public class Funcionario
+{
+  public int Id { get; set; }
+  public string Nome { get; set; }
+  public string Senha { get; set; }
+  public decimal Salario { get; set; }
+  public Permissoes Permissoes { get; set; }
+
+}
