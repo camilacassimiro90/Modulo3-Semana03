0b38ff9535acc3e952326c3808419bf2603018fd
Cami <camilacassimiro@gmail.com>
2022-09-16 10:24:13 -0300

    feature: [M3S03] Ex 2 - Criar enum Permissoes

---
+++ Enums/Permissoes.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RH.Enums
+{
+  public enum Permissoes
+  {
+    [Display(Name = "Funcionário")]
+    Funcionario = 1,
+    [Display(Name = "Gerente")]
+    Gerente,
+    [Display(Name = "Administrador")]
+    Adm
+  }
+}
