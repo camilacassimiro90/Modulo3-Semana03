691402c581d2a3922a9eefee72f8a0d28478613e
Cami <camilacassimiro@gmail.com>
2022-09-20 16:11:26 -0300

    Criar uma classe chamada 'Settings' com um atributo string estático chamado Secret. Defina algum valor a este atributo.

--- Program.cs
+++ Program.cs
@@ -12,25 +12,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var key = Encoding.ASCII.GetBytes(Settings.Secret);
+// var key = Encoding.ASCII.GetBytes(Settings.Secret);
 
 builder.Services.AddAuthentication(x =>
 {
   x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
   x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(x =>
-{
-  x.RequireHttpsMetadata = false;
-  x.SaveToken = true;
-  x.TokenValidationParameters = new TokenValidationParameters
-  {
-    ValidateIssuerSigningKey = true,
-    IssuerSigningKey = new SymmetricSecurityKey(key),
-    ValidateIssuer = false,
-    ValidateAudience = false
-  };
 });
+// .AddJwtBearer(x =>
+// {
+//   x.RequireHttpsMetadata = false;
+//   x.SaveToken = true;
+//   x.TokenValidationParameters = new TokenValidationParameters
+//   {
+//     ValidateIssuerSigningKey = true,
+//     IssuerSigningKey = new SymmetricSecurityKey(key),
+//     ValidateIssuer = false,
+//     ValidateAudience = false
+//   };
+// });
 
 
 var app = builder.Build();
---
+++ Security/Settings.cs
@@ -0,0 +1,7 @@
+namespace RH.Security;
+
+public static class Settings
+{
+  public static readonly string Secret = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJlbWFpbCI6ImF5bGFuQGJvc2Nhcm";
+
+}
